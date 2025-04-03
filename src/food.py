89258e4d47da752cd855bd4d90aca32f83a6a418
BaoPhuc <lhbaophuc0802@gmail.com>
2025-04-03 15:55:24 +0700

    Handle movement and collision

--- src/food.py
+++ src/food.py
@@ -1,19 +1,19 @@
 import random
 
 class Food:
-    def __init__(self, screen_width, screen_height, cell_size):
-        self.screen_width = screen_width
-        self.screen_height = screen_height
+    def __init__(self, width, height, cell_size):
+        self.width = width
+        self.height = height
         self.cell_size = cell_size
-        self.position = self.random_position()
+        self.position = self.generate_random_position()
 
-    def random_position(self):
-        x = random.randrange(0, self.screen_width, self.cell_size)
-        y = random.randrange(0, self.screen_height, self.cell_size)
+    def generate_random_position(self):
+        x = random.randint(0, (self.width - self.cell_size) // self.cell_size) * self.cell_size
+        y = random.randint(0, (self.height - self.cell_size) // self.cell_size) * self.cell_size
         return (x, y)
+
+    def spawn_new_food(self):
+        self.position = self.generate_random_position()
 
     def get_position(self):
         return self.position
-
-    def spawn_new_food(self):
-        self.position = self.random_position()
--- src/snake.py
+++ src/snake.py
@@ -1,23 +1,43 @@
 class Snake:
-    def __init__(self, initial_position):
-        self.body = [initial_position]
-        self.direction = (1, 0)
+    def __init__(self, start_pos, cell_size=30, width=600, height=400):
+        self.body = [start_pos]
+        self.direction = (cell_size, 0)
+        self.cell_size = cell_size
+        self.width = width
+        self.height = height
+        self.grow_next_move = False  
 
     def move(self):
         head_x, head_y = self.body[0]
-        new_head = (head_x + self.direction[0], head_y + self.direction[1])
-        self.body = [new_head] + self.body[:-1]
+        dir_x, dir_y = self.direction
+        new_head = (head_x + dir_x, head_y + dir_y)
+
+        if self.is_out_of_bounds(new_head) or new_head in self.body:  # Thêm kiểm tra tự cắn vào thân
+            return False  
+
+        self.body.insert(0, new_head)
+        
+        if not self.grow_next_move:
+            self.body.pop()  
+        else:
+            self.grow_next_move = False  
+
+        return True  
+
+    def is_out_of_bounds(self, position):
+        x, y = position
+        return x < 0 or x >= self.width or y < 0 or y >= self.height
+
+    def set_direction(self, new_direction):
+        opposite = (-self.direction[0], -self.direction[1])
+        if new_direction != opposite:
+            self.direction = new_direction
 
     def grow(self):
-        tail_x, tail_y = self.body[-1]
-        new_tail = (tail_x - self.direction[0], tail_y - self.direction[1])
-        self.body.append(new_tail)
-
-    def change_direction(self, new_direction):
-        self.direction = new_direction
-
-    def get_head(self):
-        return self.body[0]
+        self.grow_next_move = True  
 
     def get_body(self):
         return self.body
+
+    def get_head(self):
+        return self.body[0]
